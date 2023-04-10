587d37e581ab726249bc23ec352b22616ea1792a
Soham Nair <nairsam10@gmail.com>
2023-04-10 17:11:41 -0400

    Login and Registration

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 
 const presidents = require('./presidents');
+const home = require('./home');
 
 
 const constructorMethod = (app) => {
-  app.use('/', presidents);
+  app.use('/', home);
+  app.use('/presidents', presidents);
   app.use('*', (req, res) => {
     return res.status(404).render('./errorPage', {title: "Error", error1: "Oops! the page you are searching doesn't exist"});
     
--- routes/presidents.js
+++ routes/presidents.js
@@ -1,13 +1,17 @@
 const express = require('express');
-const router=express.Router();
+const router = express.Router();
 const index = require('../data/index');
 
 router.route('/')
 .get(async (req,res)=>{
     try{
-        let data = await index.presidents.getAllPresidents();
-        // console.log(data);
-        return res.render('./home',{title:'KnowYourPresidents', head:'Know Your Presidents',data:data});
+        if (!req.session.user) {
+            return res.redirect('/sign-in');
+        }
+        else {
+            let data = await index.presidents.getAllPresidents();
+            return res.render('./allPresidents',{title:'KnowYourPresidents', head:'Know Your Presidents',data:data});
+        }
     }catch(e){
         return res.status(500).render('./errorPage',{title:'Error',head:e});
     }
@@ -16,8 +20,13 @@ router.route('/')
 router.route('/:id')
 .get(async(req,res)=>{
     try {
-        let data = await index.presidents.getPresident(req.params.id);
-        return res.render('./presidentInfo',{title:data.name,head:data.name,data:data});
+        if (!req.session.user) {
+            return res.redirect('/sign-in');
+        }
+        else {
+            let data = await index.presidents.getPresident(req.params.id);
+            return res.render('./presidentInfo',{title:data.name,head:data.name,data:data});
+        }
     } catch (e) {
         return res.status(404).render('./errorPage',{title:'Error',head:e});
     }
